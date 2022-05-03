b3bc4115de5ef5b495c2bd73bbc5507c30321631
kaizerenrique <kayserenrique@gmail.com>
2022-05-03 10:20:36 -0400

    abrir en pagina nueva

--- resources/views/consultaCedula/resultados.blade.php
+++ resources/views/consultaCedula/resultados.blade.php
@@ -32,7 +32,7 @@
                                     <td class="rounded border px-4 py-2">{{$historial->nombreLaboratorio}}</td> 
                                     <td class="rounded border px-4 py-2">                                        
                                         <x-jet-button class="bg-green-500 hover:bg-green-700">
-                                            <a href="{{ route('consulta.verDocumento', $historial->nombreArchivo)}}">
+                                            <a href="{{ route('consulta.verDocumento', $historial->nombreArchivo)}}" target="_blank">
                                                 {{ __('Ver') }}
                                             </a>                                
                                         </x-jet-button>                                                                 
